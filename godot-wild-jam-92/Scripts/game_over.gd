8b575bac1c0677f920c09a2025462a92e465b33e
XMNNguyen <rupyman95@gmail.com>
2026-04-19 17:48:16 -0700

    added score and highscore to gameover screen

--- godot-wild-jam-92/Scripts/game_over.gd
+++ godot-wild-jam-92/Scripts/game_over.gd
@@ -3,10 +3,15 @@ extends Node2D
 
 
 func _ready() -> void:
+	if global.high_score < global.score:
+		global.high_score = global.score
+	
+	$Label2.text = "SCORE: " + str(global.score) + "\nHIGHSCORE: " + str(global.high_score)
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 
 func _on_retry_button_pressed() -> void:
+	global.score = 0
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
