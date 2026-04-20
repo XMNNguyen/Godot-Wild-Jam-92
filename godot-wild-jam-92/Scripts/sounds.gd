34b15697853388c11a375e86db98e3359952a3e5
XMNNguyen <rupyman95@gmail.com>
2026-04-19 17:17:24 -0700

    set up audio bus

---
+++ godot-wild-jam-92/Scripts/sounds.gd
@@ -0,0 +1,10 @@
+class_name Sounds
+extends Node
+
+@onready var jump_sound: AudioStreamPlayer3D = $jump
+@onready var collect_sound : AudioStreamPlayer3D = $collect
+@onready var dash_sound : AudioStreamPlayer3D = $dash
+@onready var hit_sound : AudioStreamPlayer3D = $hit
+@onready var gameover : AudioStreamPlayer3D = $gameover
+@onready var sizzling : AudioStreamPlayer3D = $sizzling
+@onready var throw : AudioStreamPlayer3D = $throw
